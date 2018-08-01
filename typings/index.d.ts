2c6e082e9154fced4957cf691f68fc3c753a6df0
hhh <1340545944@qq.com>
2018-08-01 18:17:37 +0800

    1.2.0

--- typings/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const Time: {
-    replacers: {
-        [id: string]: (date: Date) => string;
-    };
-    get(format: string): string;
-    format(date: Date, format: string): string;
-};
-export = Time;
-//# sourceMappingURL=index.d.ts.map
--- typings/replacers.d.ts
+++
@@ -1,5 +0,0 @@
-declare const replacers: {
-    [id: string]: (date: Date) => string;
-};
-export = replacers;
-//# sourceMappingURL=replacers.d.ts.map
